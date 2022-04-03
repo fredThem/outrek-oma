d77d46f2cf4789c97bdfd13a0f667907f2888207
Frederic Desmarais <frederic.desmarais@radio-canada.ca>
2022-04-02 20:10:54 -0400

    Adding Polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
